572c798a67da3cb11120cef2084f3571bf6c295d
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-05-12 17:23:47 -0600

    Basic constructor and configuration class

---
+++ ParksRecClient/ParksRecClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.Http;
+
+namespace ParksRec.Client
+{
+    public class ParksRecClient
+    {
+        public ParksRecConfig ParksRecConfig { get; set; }
+
+        public ParksRecClient(ParksRecConfig config)
+        {
+            ParksRecConfig = config;
+
+            HttpClientFactory httpClientFactory = new HttpClientFactory();
+            httpClientFactory.Register("ParksRecClient", builder => builder
+                .ConfigureHttpClient(c => c.BaseAddress = ParksRecConfig.Uri)
+                .ConfigureHttpClient(c => c.DefaultRequestHeaders.Add("Authorization", $"Bearer {ParksRecConfig.Token}"))
+                .ConfigureHttpClient(c => c.DefaultRequestHeaders.Add("Accept", "application/json")));
+
+            HttpClient httpClient = httpClientFactory.CreateClient("ParksRecClient");
+
+
+        }
+    }
+}
---
+++ ParksRecClient/ParksRecConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParksRec.Client
+{
+    public class ParksRecConfig
+    {
+        public Uri Uri { get; } = new Uri("https://parks-and-rec-api.herokuapp.com/api/v1/");
+        public string Token { get; set; }
+    }
+}
