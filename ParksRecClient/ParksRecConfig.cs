20d81c3247a057a7fa07f852f440ce90b1ae7105
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-05-12 21:33:21 -0600

    Change to correct Uri

--- ParksRecClient/ParksRecConfig.cs
+++ ParksRecClient/ParksRecConfig.cs
@@ -6,7 +6,7 @@ namespace ParksRec.Client
 {
     public class ParksRecConfig
     {
-        public Uri Uri { get; } = new Uri("https://parks-and-rec-api.herokuapp.com/api/v1/");
+        public Uri Uri { get; } = new Uri("https://parksandrecapi.com/api/v1/");
         public string Token { get; set; }
     }
 }
