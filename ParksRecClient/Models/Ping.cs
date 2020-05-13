3d8b94b6ae5d6f064de0f9c8e2f6b3cf3a1b27db
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-05-12 18:28:46 -0600

    Added ping model

---
+++ ParksRecClient/Models/Ping.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ParksRec.Client.Models
+{
+    public class Ping
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+        
+        [JsonProperty("date")]
+        public DateTime DateTime { get; set; }
+    }
+}
