86a2f49199cfbb99accb599beb3900e685168203
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-05-12 20:47:28 -0600

    Added props to client

--- ParksRecClient/ParksRecClient.cs
+++ ParksRecClient/ParksRecClient.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Microsoft.Extensions.Http;
+using ParksRec.Client.Helpers;
 using System.Net.Http;
-using Microsoft.Extensions.Http;
 
 namespace ParksRec.Client
 {
     public class ParksRecClient
     {
         public ParksRecConfig ParksRecConfig { get; set; }
+        public Bands Bands { get; set; }
+        public Characters Characters { get; set; }
+        public Episodes Episodes { get; set; }
+        public Seasons Seasons { get; set; }
 
         public ParksRecClient(ParksRecConfig config)
         {
@@ -20,6 +24,12 @@ namespace ParksRec.Client
 
             HttpClient httpClient = httpClientFactory.CreateClient("ParksRecClient");
 
+            Requests request = new Requests(httpClient);
+
+            Bands = new Bands(request);
+            Characters = new Characters(request);
+            Episodes = new Episodes(request);
+            Seasons = new Seasons(request);
 
         }
     }
